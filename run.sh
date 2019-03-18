7f51320b4500d2648f5f4f55594c7bf083d48122
savilovoa <savilovoa@gmail.com>
2019-03-18 14:27:12 +0300

    config

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
-docker run --name 1c_logs --restart=always \
+docker run -it --name 1c_logs --restart=always \
   -v /var/lib/1c_logs/since:/var/lib/1c_logs/since \
   -v /data/1c_logs/erp/1Cv8Log:/var/lib/1c_logs/data \
   -v /var/log/1c_logs:/var/log/1c_logs \
-  savilovoa/1c_logs
+  savilovoa/1c_logs bash
