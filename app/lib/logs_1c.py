f43c9f74f729e08cb3bdec5b1c99015d7512ccee
РЎР°РІРёР»РѕРІ РћР»РµРі РђР»РµРєСЃР°РЅРґСЂРѕРІРёС‡ <savilovoa@limak.ru>
2019-03-18 10:45:38 +0300

    fix error

--- app/lib/logs_1c.py
+++ app/lib/logs_1c.py
@@ -102,7 +102,7 @@ class scan_1c_logs(object):
             self.sincefilename = os.path.join(config.get("GLOBAL", "dirsince"), dbname + ".since")
         else:
             self.sincefilename = dbname + ".since"
-
+        logger.info("since: ".format(self.sincefilename))
         self.since_load()
 
 
@@ -477,8 +477,8 @@ class scan_1c_logs(object):
                                     break
 
         except Exception:
-            logger.error(str(Exception))
-            logger.exception()
+            logger.error(str(Exception), exc_info=True)
+            
             res = False
         finally:
             if not res:
